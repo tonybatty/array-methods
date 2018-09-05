788b373bccf35e033ad9b38335a3f8a0ca3e4217
dmitrigrabov <d.grabov@gmail.com>
2018-09-05 08:47:29 +0100

    Add exercises

---
+++ src/index.js
@@ -0,0 +1,304 @@
+function removeMiddle( words ){
+  // words is an array which contains an odd number of strings
+  // return a new array containing only the middle word
+  // the words array should no longer contain the middle word
+}
+
+function get2ndAnd3rd( myArray ){
+  // myArray is an array of numbers
+  // return an array containing the 2nd and 3rd items from myArray
+  // myArray should remain unchanged
+}
+
+function stringify( myArray ){
+  // myArray is an array of numbers
+  // return a new array which has all items converted to strings
+  // myArray should remain unchanged
+}
+
+function types(input){
+  // input is an array of items of all types
+  // return an array which contains the
+  // type of each of the items in input array
+}
+
+function wordLengths( words ){
+  // words is an array of strings
+  // return a new array that contains the number of letters in each word
+  // for example
+  // input:
+  // [ 'jupiter', 'mars', 'saturn' ]
+  // output:
+  // [ 7, 4, 6]
+}
+
+function cities( capitals, formatter ){
+  // capitals is an array of objects that have a city and country property
+  // for example
+  // {
+  //   city: 'Paris',
+  //   country: 'France'
+  // }
+  // formatter is a callback function provided for you which
+  // transforms a capital object into a sentence returns it such as
+  // 'Paris is the capital of France'.
+  // Apply formatter to each object in capitals array and
+  // return an array of resulting sentences
+}
+
+function largerThanTen( numbers ){
+  // numbers is an array of numbers
+  // return a new array that contains only numbers
+  // from the input array which are greater than 10
+}
+
+function even( numbers ){
+  // numbers is an array of numbers
+  // return a new array that contains only even numbers from the input array
+}
+
+function findTheNeedle( words ){
+  // words is an array of words
+  // return the index of the word 'needle'
+}
+
+function findLargest( numbers ){
+  // numbers is an array of numbers
+  // return the largest number from that array
+}
+
+function addAllnumbers( numbers ) {
+  // numbers is an array of numbers
+  // return the sum of all the numbers in the array
+}
+
+function sortingStrings(strings){
+  // strings is an array of strings
+  // sort them in alphabetical order and return the sorted array
+}
+
+function sortingNumbers(numbers){
+  // numbers is an array of numbers
+  // sort them in ascending order and return the sorted array
+}
+
+function sortingNumbersDescending(numbers){
+  // numbers is an array of numbers
+  // sort them in descending order and return the sorted array
+}
+
+function sortingCars(cars){
+  // a car object has a make, model, year. For example
+  // const car = {
+  //   make: 'Ford',
+  //   model: 'Fiesta',
+  //   year: 1994
+  // }
+  //
+  // cars is an array of car objects. Sort them ascending by year and return
+  // the sorted array.
+}
+
+function deleteColour( car ){
+  // car is an object with properties make, model and colour. For example
+  // {
+  //   make: 'Ford',
+  //   model: 'Fiesta',
+  //   colour: 'red'
+  // }
+
+  // delete the property colour and return car without this property
+};
+
+function paintShop( cars, colour ){
+  // cars is an array of objects that have
+  // their properties are `make`, `model` and `colour`
+
+  // for example
+  // {
+  //   make: 'Ford',
+  //   model: 'Fiesta',
+  //   colour: 'Red'
+  // }
+
+  // set the colour of each Ford car to be the colour
+  // passed in and return the new array
+
+  // the original array passed in should not change
+  // hint: look up 'Cloning objects in JavaScript'
+}
+
+function secondLargest( numbers ){
+  // numbers is an array of numbers
+  // return the index of the second
+  // largest number in the array
+}
+
+function addSales( city, sales ){
+  // Argument city is a string containing city name
+  // Argument sales is a integer containing the sales
+  // for that location
+
+  // We have object containing sales
+  const globalSales = {
+    london: 200,
+    paris: 300,
+    berlin: 150,
+    madrid: 400
+  };
+
+  // If city already exists in object, add sales figure
+  // to its total. If city does not exist, create a new
+  // property and save the sales figure as its value.
+
+  // return the updated globalSales object
+}
+
+function totalSales( sales ){
+  // You are passed a sales object similar to the one
+  // in the previous exercise. Add up all the sales figures
+  // and return the total.
+
+  // Hint: look up how to get the values of an object
+}
+
+function highestSum( input ){
+  // input is an array of arrays of numbers.
+  // For example
+  // [
+  //   [ 4, 3, 2],
+  //   [ 7, 1],
+  //   [ 9, 11, 15, 1]
+  // ]
+
+  // If an inner array is empty, consider its sum to be 0
+
+  // find and return the largest sum of an inner array
+}
+
+function walletSum( wallet ){
+  // A wallet object has keys which are the denominations
+  // and values which are the number of those notes in the
+  // wallet. See example below.
+  //
+  // const exampleWallet = {
+  //   5: 3,
+  //   10: 7,
+  //   20: 2
+  // };
+  //
+  // calculate the sum of money in the wallet and return
+  // the total.
+
+  // the denominations used in this exercise are 5, 10 and 20
+}
+
+function walletMerge( wallet1, wallet2 ){
+  // return a new wallet object containing the contents of
+  // both wallets passed in.
+
+  // the denominations used in this exercise are 5, 10 and 20
+}
+
+function arrayOfWallets( wallets ){
+  // wallets is an array of wallets
+
+  // Return a new wallet object containing the notes from all wallets
+  // the denominations used in this exercise are 5, 10 and 20
+}
+
+function crazyMoney( wallets ){
+  // In previous exercises the notes denominations were
+  // limited to 5, 10 and 20. We now have wallets that
+  // can have notes of any denomination. Implement a function
+  // which accepts an array of random sized notes and
+  // calculate the total amount of money in them.
+
+  // Hint: look up how to get keys of objects. You may also need
+  // to use square bracket notation to look up values which
+  // correspond to those keys
+}
+
+/*
+ * STRETCH GOALS
+ */
+
+function map(array, callback){
+  // Implement a map function which accepts an array of items and a callback function
+  // It should call the callback with each item from the array
+  // and return a new array that contains the output of each callback
+
+  // You will probably need to use a for loop for this. Do not use the
+  // JavaScript map array method
+
+  // Implement your own unit tests, let us know if you need any help
+}
+
+function filter(array, callback){
+  // Implement a filter function which accepts an array of items and a callback function
+  // It should call the callback with each item from the array
+  // and return a new array that the items from the array for which the
+  // callback returns a truthy value
+
+  // You will probably need to use a for loop for this. Do not use the
+  // JavaScript filter array method
+
+  // Implement your own unit tests, let us know if you need any help
+}
+
+function find(array, callback){
+  // Implement a find function which accepts an array of items and a callback function
+  // It should return the first item from the array for which
+  // the callback returns a truthy value
+
+  // You will probably need to use a for loop for this. Do not use the
+  // JavaScript find array method
+
+  // Implement your own unit tests, let us know if you need any help
+}
+
+function reduce(array, reducer, initialAcc){
+  // Implement a reduce function which accepts an array of items, a reducer function
+  // and an initial value of the acc.
+  // It should return the last value that was output by the reducer.
+
+  // The reducer expects to receive an acc and current item from the array.
+  // The initial value of the acc is passed as an argument to this function
+
+  // You will probably need to use a for loop for this. Do not use the
+  // JavaScript reduce array method
+
+  // Implement your own unit tests, let us know if you need any help
+}
+
+module.exports = {
+  removeMiddle,
+  get2ndAnd3rd,
+  stringify,
+  types,
+  wordLengths,
+  cities,
+  largerThanTen,
+  even,
+  findTheNeedle,
+  findLargest,
+  addAllnumbers,
+  sortingStrings,
+  sortingNumbers,
+  sortingNumbersDescending,
+  sortingCars,
+  deleteColour,
+  paintShop,
+  secondLargest,
+  addSales,
+  totalSales,
+  highestSum,
+  walletSum,
+  walletMerge,
+  arrayOfWallets,
+  crazyMoney,
+  map,
+  filter,
+  find,
+  reduce
+};
---
+++ test/index.test.js
@@ -0,0 +1,402 @@
+const {
+  removeMiddle,
+  get2ndAnd3rd,
+  stringify,
+  types,
+  wordLengths,
+  cities,
+  largerThanTen,
+  even,
+  findTheNeedle,
+  findLargest,
+  addAllnumbers,
+  sortingStrings,
+  sortingNumbers,
+  sortingNumbersDescending,
+  sortingCars,
+  deleteColour,
+  paintShop,
+  secondLargest,
+  addSales,
+  totalSales,
+  highestSum,
+  walletSum,
+  walletMerge,
+  arrayOfWallets,
+  crazyMoney,
+  map,
+  filter,
+  find,
+  reduce
+} = require('../src');
+
+test('Remove middle', () => {
+  const words = [ 'mouse', 'giraffe', 'queen', 'window', 'bottle'];
+
+  const expectedWords = [ 'mouse', 'giraffe', 'window', 'bottle'];
+  const expectedOutput = [ 'queen' ];
+
+  const output = removeMiddle( words );
+
+  expect(output).toEqual(expectedOutput);
+  expect(words).toEqual(expectedWords);
+});
+
+test('Get second and third', () => {
+  const numbers = [ 90, 5, 11, 5, 6 ];
+
+  const expectedOutput = [ 5, 11];
+  const expectedNumbers = [ 90, 5, 11, 5, 6 ];
+
+  const output = get2ndAnd3rd( numbers );
+
+  expect(output).toEqual(expectedOutput);
+  expect(numbers).toEqual(expectedNumbers);
+});
+
+test('Convert numbers to strings', () => {
+  const myArray = [ 31, 57, 12, 5];
+
+  const unchanged = [ 31, 57, 12, 5];
+  const expected = [ '31', '57', '12', '5'];
+  const output = stringify( myArray );
+
+
+  expect(output).toEqual(expected);
+  expect(myArray).toEqual(unchanged);
+});
+
+test('Return types of items in array', () => {
+  const input = [
+    4.6,
+    false,
+    NaN,
+    ['a', 'b'],
+    null,
+    undefined
+  ];
+
+  const output = types(input);
+  expect(output).toEqual(['number', 'boolean', 'number', 'object', 'null', 'undefined']);
+
+});
+
+test('Get word lengths', () => {
+  const words = [ 'sun', 'potato', 'roundabout', 'pizza' ];
+  const expected = [ 3, 6, 10, 5 ];
+
+  const output = wordLengths( words );
+  expect(output).toEqual( expected );
+});
+
+test('Cities', () => {
+  const capitals = [
+    { city: 'Paris', country: 'France' },
+    { city: 'Madrid', country: 'Spain' },
+    { city: 'Rome', country: 'Italy' },
+  ];
+
+  function transform({city, country}){
+    return `${city} is the capital of ${country}`;
+  }
+
+  const expected = [
+    'Paris is the capital of France',
+    'Madrid is the capital of Spain',
+    'Rome is the capital of Italy'
+  ];
+
+  const result = cities(capitals, transform);
+
+  expect(result).toEqual(expected);
+});
+
+test('Get numbers greater than 10', () => {
+  const numbers = [ 4, 10, 32, 9, 21];
+  const expected = [ 32, 21 ];
+
+  const output = largerThanTen( numbers );
+  expect(output).toEqual( expected );
+});
+
+test('Get even numbers', () => {
+  const numbers = [ 22, 13, 73, 82, 4];
+  const expected = [ 22, 82, 4 ];
+
+  const output = even( numbers );
+  expect(output).toEqual( expected );
+});
+
+test('Find the needle', () => {
+  const words = [ 'house', 'train', 'slide', 'needle', 'book' ];
+  const expected = 3;
+
+  const output = findTheNeedle( words );
+  expect(output).toEqual( expected );
+});
+
+test('Find largest number', () => {
+  const numbers = [ 3, 21, 88, 4, 36];
+  const expected = 88;
+
+  const output = findLargest( numbers );
+  expect(output).toEqual( expected );
+});
+
+test('Add all numbers', () => {
+  const numbers = [ 9, 23, 10, 3, 8 ];
+  const expected = 53;
+
+  const output = addAllnumbers( numbers );
+
+  expect( output ).toEqual( 53 );
+});
+
+test('Sorting strings', () => {
+  const companies = ['netflix', 'google', 'amazon', 'facebook', 'microsoft', 'apple'];
+  const expected = ['amazon', 'apple', 'facebook', 'google', 'microsoft', 'netflix' ];
+
+  const output = sortingStrings(companies);
+
+  expect( output ).toEqual( expected );
+});
+
+test('Sorting numbers', () => {
+  const numbers = [99, -2, 0, 101, 2, 3, 22, 37, 1, -11];
+  const expected = [-11,  -2, 0, 1, 2, 3, 22, 37, 99, 101];
+
+  const output = sortingNumbers(numbers);
+
+  expect( output ).toEqual( expected );
+});
+
+test('Sorting numbers descending', () => {
+  const numbers = [99, -2, 0, 101, 2, 3, 22, 37, 1, -11];
+  const expected = [101, 99, 37, 22, 3, 2, 1, 0, -2, -11];
+
+  const output = sortingNumbersDescending(numbers);
+
+  expect( output ).toEqual( expected );
+});
+
+test('Sorting cars', () => {
+  const cars = [{
+    make: 'tesla',
+    model: 'roadster',
+    year: 2020
+  }, {
+    make: 'ford',
+    model: 'fiesta',
+    year: 1994
+  }, {
+    make: 'reliant',
+    model: 'robin',
+    year: 1973
+  }, {
+    make: 'toyota',
+    model: 'prius',
+    year: 2011
+  }];
+
+  const expected = [{
+    make: 'reliant',
+    model: 'robin',
+    year: 1973
+  }, {
+    make: 'ford',
+    model: 'fiesta',
+    year: 1994
+  }, {
+    make: 'toyota',
+    model: 'prius',
+    year: 2011
+  }, {
+    make: 'tesla',
+    model: 'roadster',
+    year: 2020
+  }];
+
+  const output = sortingCars(cars);
+
+  expect( output ).toEqual( expected );
+});
+
+test('Delete colour', () => {
+  const car = {
+    make: 'Ford',
+    model: 'Mondeo',
+    colour: 'gold'
+  };
+  const expected = {
+    make: 'Ford',
+    model: 'Mondeo',
+  };
+
+  const output = deleteColour(car);
+
+  expect( output ).toEqual( expected );
+});
+
+test('Paint shop', () => {
+  const cars = [
+    { make: 'Ford', model: 'Fiesta', colour: 'Red' },
+    { make: 'Land Rover', model: 'Defender', colour: 'Muddy' },
+    { make: 'Toyota', model: 'Prius', colour: 'Silver' },
+    { make: 'Honda', model: 'Civic', colour: 'Yellow' }
+  ];
+
+  const unpaintedCars = [
+    { make: 'Ford', model: 'Fiesta', colour: 'Red' },
+    { make: 'Land Rover', model: 'Defender', colour: 'Muddy' },
+    { make: 'Toyota', model: 'Prius', colour: 'Silver' },
+    { make: 'Honda', model: 'Civic', colour: 'Yellow' }
+  ];
+
+  const repaintedCars = [
+    { make: 'Ford', model: 'Fiesta', colour: 'Pink' },
+    { make: 'Land Rover', model: 'Defender', colour: 'Muddy' },
+    { make: 'Toyota', model: 'Prius', colour: 'Silver' },
+    { make: 'Honda', model: 'Civic', colour: 'Yellow' }
+  ];
+
+  const output = paintShop( cars, 'Pink' );
+
+  expect( output ).toEqual( repaintedCars );
+  expect( cars ).toEqual( unpaintedCars );
+});
+
+test('Second largest', () => {
+  const numbers = [ 2, 0, 23, 0, 57, 1 ];
+
+  const output = secondLargest( numbers );
+
+  expect( output ).toEqual( 2 );
+});
+
+test('Add sales', () => {
+  const londonx = {
+    london: 250,
+    paris: 300,
+    berlin: 150,
+    madrid: 400
+  };
+
+  const londonOutput = addSales( 'london', 50 );
+  expect( londonOutput ).toEqual( londonx );
+
+  const barcelonax = {
+    london: 200,
+    paris: 300,
+    berlin: 150,
+    madrid: 400,
+    barcelona: 200
+  };
+
+  const barcelonaOutput = addSales( 'barcelona', 200 );
+  expect( barcelonaOutput ).toEqual( barcelonax );
+});
+
+test('Total sales', () => {
+  const output = totalSales({
+    london: 200,
+    paris: 100,
+    berlin: 150,
+    oslo: 50
+  });
+
+  expect( output ).toEqual( 500 );
+});
+
+test('Highest sum - 1', () => {
+  const input = [
+    [ 4, 3, 2],
+    [ 7, 1],
+    [ 9, 11, 15, 1]
+  ];
+
+  const output = highestSum(input);
+
+  expect(output).toEqual( 36 );
+});
+
+test('Highest sum - 2', () => {
+  const input = [
+    [ 12, 3, 4],
+    [],
+    [ 21, 7],
+    [ 9, 5, 3]
+  ];
+
+  const output = highestSum(input);
+
+  expect(output).toEqual( 28 );
+});
+
+test('Wallet sum', () => {
+  const output = walletSum({
+    5: 3,
+    10: 8,
+    20: 5
+  });
+
+  expect( output ).toEqual( 195 );
+});
+
+test('Wallet merge', () => {
+  const output = walletMerge({
+    5: 2,
+    10: 3,
+    20: 1
+  }, {
+    5: 4,
+    10: 7,
+    20: 3
+  });
+
+  expect( output ).toEqual({
+    5: 6,
+    10: 10,
+    20: 4
+  });
+});
+
+test('Array of wallets', () => {
+  const output = arrayOfWallets([{
+    5: 2,
+    10: 3,
+    20: 8
+  }, {
+    5: 2,
+    10: 1,
+    20: 0
+  },{
+    5: 0,
+    10: 3,
+    20: 4
+  }]);
+
+  expect( output ).toEqual({
+    5: 4,
+    10: 7,
+    20: 12
+  });
+});
+
+test('Crazy money', () => {
+  const output = crazyMoney([{
+    2: 1,
+    7: 3,
+    14: 2,
+    15: 2
+  }, {
+    1: 3,
+    4: 2,
+    5: 2,
+    9: 0
+  }, {
+    2: 2,
+    15: 3
+  }]);
+
+  expect( output ).toEqual( 151 );
+});
